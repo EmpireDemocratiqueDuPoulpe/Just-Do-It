f9e00b1dd579a34a0d4d1c8700bae0ebab8f8088
Alexis <293287@supinfo.com>
2020-03-19 14:54:24 +0100

    Ajout de la page index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once "./init.php";
+
+############################
+# Check if user is connected
+############################
+
+if (!$is_connected) redirectTo("./login.php");
+
+############################
+# Import the view
+############################
+
+require_once "./views/index_v.php";
+
+var_dump($_SESSION);
